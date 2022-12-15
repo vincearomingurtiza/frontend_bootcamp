fce5b99aafcbcdc0e1613f9b1a4f46ae005fcaa4
iisaiaaah <vincearomingurtiza@gmail.com>
2022-12-15 10:26:48 +0800

    Summary: Update 3: Multiplication Table Viewing Update (Horizontal)

--- Duplicate Box/assets/js/custom/script.js
+++ Duplicate Box/assets/js/custom/script.js
@@ -24,12 +24,12 @@ function activityTwo(){
     $("#act2_list").empty();
 
     for (var count_start = 1; count_start <= selected_value; count_start++){
-            $("#act2_list").removeClass("no_data");
+        $("#act2_list").removeClass("no_data");
 
-            let box_duplicate = $(".act2_item:first").clone();
-            box_duplicate.removeClass("clone_item");
-            $("#act2_list").append(box_duplicate);
-        }
+        let box_duplicate = $(".act2_item:first").clone();
+        box_duplicate.removeClass("clone_item");
+        $("#act2_list").append(box_duplicate);
+    }
 };
 
 function deleteButton(){
@@ -40,13 +40,13 @@ function deleteButton(){
 
 function activityThree(){
     let selected_value  = $("#act3_select option:selected").val();
-    let count_start;
-    $(".byone").addClass("add_height_on_list");
-    for(count_start=1; count_start <= 10; count_start++){ 
-        let result = count_start * selected_value;
-        // let result_nine = count_start * 9;
-        $(".byone").append("<li> " + result + "</li>");
+    $(".multiplier_list").empty();
+    
+    for (var count_start = 1; count_start <= selected_value; count_start++){
+
+        for (multiply_by = 1; multiply_by <= 10; multiply_by++){ 
+            let sum = multiply_by * count_start;
+            $(".multiplier_list").append("<li> " + sum + "</li>");
+        }
     }
 };
-
-// pag pili ng selected value mag for l
